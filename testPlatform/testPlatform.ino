a68e01120c38389ec7c9a2df266769497e73ca20
Bruno Sampaio Camba <brunocamba@gmail.com>
2016-07-17 14:24:36 -0300

    Created a test platform to simulate the ground station.

---
+++ testPlatform/testPlatform.ino
@@ -0,0 +1,67 @@
+/*  Project Canastra (Team Gama) - Test Platform for the ground station
+  The test platform is reading from potentiometers to simulate
+  ground station received values being transmitted to the software.
+
+  Values printed to serial are raw and need processing.
+
+  Version 1.0
+*/
+
+// Variable Declarations
+int Timestamp;
+int TeamID;
+int R_Temp_Ext;
+int R_Temp_Int;
+int R_Lum_Ext;
+int R_Press_Ext;
+int R_GPS_Lat;
+int R_GPS_Elev;
+int R_GPS_Long;
+
+void setup() {
+  //Initialize Serial Connection
+  Serial.begin(9600);
+
+  //Initialize variables
+  Timestamp = 0;
+  TeamID = 3;
+}
+
+void loop() {
+
+  //Read from sensors
+  R_Temp_Ext = analogRead(A0);
+  R_Temp_Int = analogRead(A1);
+  R_Lum_Ext = analogRead(A2);
+  R_Press_Ext = analogRead(A3);
+  R_GPS_Lat = analogRead(A4);
+  R_GPS_Elev = analogRead(A5);
+  R_GPS_Long = R_GPS_Lat; //GPS_Lat = GPS_Long for simplicity:
+                          //using only 6 potentiometers.
+
+
+  //Print to Serial
+  printLabel("Timestamp: ",Timestamp);
+  printLabel("TeamID: ",TeamID);
+  printLabel("External Temperature: ", R_Temp_Ext);
+  printLabel("Internal Temperature ", R_Temp_Int);
+  printLabel("External Luminosity ", R_Lum_Ext);
+  printLabel("External Pressure ", R_Press_Ext);
+  printLabel("External Luminosity ", R_Lum_Ext);
+  printLabel("GPS Latitude: ", R_GPS_Lat);
+  printLabel("GPS Longitude: ", R_GPS_Long);
+  printLabel("GPS Elevation: ", R_GPS_Elev);
+  Serial.println();
+
+  //Update fake timestamp
+  Timestamp++;
+}
+
+
+//Function to print the Label followed by the sensor value
+//in the same line.
+void printLabel(char Label[],int Sensor) {
+  String print = String(Label);
+  print += Sensor;
+  Serial.println(print);
+}
